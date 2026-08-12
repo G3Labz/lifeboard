080404e68abc33ef7b90fec21baa57c60fb76894
Gabriel Cruz <cruzg3dev@gmail.com>
2026-08-12 00:32:33 -0300

    feat: register submodules and lifeboard-multi-php

--- lifeboard-multi-php/conecta.php
+++
@@ -1,4 +0,0 @@
-<?php
-#$conexao = mysqli_connect("databases.000webhost.com", "id5629265_g3", "lojaloja", "id5629265_loja");
-
-$conexao = mysqli_connect("localhost", "root", "", "loja");
--- lifeboard-multi-php/js/relogio.js
+++
@@ -1,9 +0,0 @@
-function relogio(){
-	var data = new Date();
-	var horas = data.getHours();
-	var minutos = data.getMinutes();
-	var segundos = data.getSeconds();
-	var exibe = document.getElementById("horas");
-	exibe.innerHTML = "São " + horas + ":" + minutos + ":" + segundos;
-}
-setInterval(relogio, 0001);
